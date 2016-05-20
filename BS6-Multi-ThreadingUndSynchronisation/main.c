b205f777e0ff0377963c40c444b3858d1a28a81d
ozhurakovskaya <oxana.zhurakovskaya@fh-bielefeld.de>
2016-05-20 14:04:23 +0200

    srct job & read thread

--- BS6-Multi-ThreadingUndSynchronisation/main.c
+++ BS6-Multi-ThreadingUndSynchronisation/main.c
@@ -1,10 +1,11 @@
 #include "queue.h"
 #include "miniz.h"
 #include <pthread.h>
+#include <dirent.h>
 
 typedef struct job{
-	char *dir;
-
+	char *path;
+	char *filename;
 
 }Job;
 static Queue jobQueue;
@@ -14,4 +15,11 @@ int main(int argc, char *argv[]){
 
 
 	return 0;
+}
+
+int read(const char *path){
+	DIR *dir = opendir (path);
+	if(!dir){
+		return -1;
+	}
 }
