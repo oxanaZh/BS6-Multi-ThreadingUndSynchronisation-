163b5063c138f4c7929f3abb0021731ea8899394
ozhurakovskaya <oxana.zhurakovskaya@fh-bielefeld.de>
2016-05-25 13:13:57 +0200

    compress function fnktioniert, pfade werden richtig gesetzt

--- BS6-Multi-ThreadingUndSynchronisation/main.c
+++ BS6-Multi-ThreadingUndSynchronisation/main.c
@@ -21,17 +21,46 @@ typedef struct job {
 } Job;
 static Queue jobQueue;
 
-void *readPath(char *path);
-void *compileFiles();
+void* readPath(void* path) {
+	char resolved_path[MAX_PATH];
+	DIR *dir = NULL;
+	struct dirent *dptr = NULL;
+	char* dot = ".";
+	char* dotdot = "..";
+	if (realpath((char*) path, resolved_path)) {
+		printf("resolved_path: %s\n", resolved_path);
+		if ((dir = opendir(resolved_path))) {
+			while ((dptr = readdir(dir))) {
+				if (strcmp(dptr->d_name, dot) == 0
+						|| strcmp(dptr->d_name, dotdot) == 0) {
+					continue;
+				}
+				Job *j = (Job *) malloc(sizeof(Job));
+
+				if (!j) {
+					exit(EXIT_FAILURE);
+				}
+				setJob(resolved_path, dptr->d_name, j);
+				queue_insert(jobQueue, j);
+				printf("file: %s\n", j->filename);
+			}
+			closedir(dir);
+		}
+	}
+	return NULL;
+}
+void *compileFiles(void *arg);
+void setJob(char *path, char *file, Job* job);
 
 int main(int argc, char *argv[]) {
 	jobQueue = queue_create();
 	printf("argv[1]: %s\n", argv[1]);
 
 	pthread_t reader;
-	int error = pthread_create(&reader, NULL, readPath, (void*)argv[1]);
-	pthread_join(reader,NULL);
-
+	//int error = pthread_create(&reader, NULL, readPath, (void*) argv[1]);
+	//pthread_join(reader, NULL);
+	int error = 0;
+	readPath(argv[1]);
 	if (error != 0)
 		exit(EXIT_FAILURE);
 
@@ -39,8 +68,7 @@ int main(int argc, char *argv[]) {
 
 	int threads_ids[COMPILETHREADS];
 
-	pthread_mutex_init(&lock,NULL);
-
+	pthread_mutex_init(&lock, NULL);
 
 	int i;
 
@@ -60,41 +88,7 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void *readPath(char *path) {
-	char resolved_path[MAX_PATH];
-	DIR *dir = NULL;
-	struct dirent *dptr = NULL;
-	char *dot = ".";
-	char *dotdot = "..";
-	char * compressed = ".compr";
-	if (realpath(path, resolved_path)) {
-		printf("resolved_path: %s\n", resolved_path);
-		if ((dir = opendir(resolved_path))) {
-			while ((dptr = readdir(dir))) {
-				if (strcmp(dptr->d_name, dot) == 0
-						|| strcmp(dptr->d_name, dotdot) == 0) {
-					continue;
-				}
-				Job *j = (Job *) malloc(sizeof(Job));
-
-				if (!j)
-					exit(EXIT_FAILURE);
-				j->path = resolved_path;
-				strcat(j->path,"/");
-
-				j->filename = j->path;
-				strcat(j->path,dptr->d_name);
-				j->newfilename = j->filename;
-				strcat(j->newfilename,compressed);
-				queue_insert(jobQueue, j);
-				printf("file: %s\n", j->filename);
-			}
-			closedir(dir);
-		}
-	}
-	return NULL;
-}
-void *compileFiles() {
+void *compileFiles(void * arg) {
 	pthread_mutex_lock(&lock);
 	Job *j;
 
@@ -116,7 +110,7 @@ void *compileFiles() {
 	char * absoluteFilename = j->filename;
 	printf("absoluteFilename %s\n",absoluteFilename);
 	char * newfile = j->newfilename;
-	printf("newfile %s\n",newfile);
+	printf("compress newfile %s\n",newfile);
 
 	read = fopen (absoluteFilename, "r");
 	write =fopen (newfile, "w+");
@@ -126,16 +120,44 @@ void *compileFiles() {
 	}
 	const int lineLength = 255;
 	char buff[lineLength];
-
+	Result *r = compress_string(buff);
 	while(fgets(buff, lineLength, (FILE*)read)){
-		Result *r = compress_string(buff);
+
 		fputs(r->data,(FILE*)write );
-		free(r->data);
-		free(r);
+
 	}
+	free(r->data);
+			free(r);
 	fclose(read);
 	fclose(write);
 
 	return NULL;
+
+}
+
+void setJob(char *path, char *file, Job* job){
+	printf("setJob----- \n\n");
+	char * compressed = ".compr";
+	printf("path: %s\n",path);
+	printf("filename: %s\n",file);
+	printf("job path: %s\n",job->path);
+
+	job->path = (char *) malloc(MAX_PATH);
+	strcpy(job->path, path);
+
+	job->filename = (char *) malloc(MAX_PATH);
+	strcpy(job->filename,job->path);
+
+	strcat(job->filename,"/");
+	strcat(job->filename,file);
+
+	job->newfilename = (char *) malloc(MAX_PATH);
+	strcpy(job->newfilename,job->filename);
+	strcat(job->newfilename,compressed);
+
+	printf("job path: %s\n",job->path);
+	printf("job filename: %s\n",job->filename);
+	printf("job newfilename: %s\n\n",job->newfilename);
+
 
 }
